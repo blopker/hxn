6c14b033cca6ee5c78fcde0f7bd253d503d369a0
Bo Lopker <blopker@23andme.com>
2015-06-04 00:33:46 -0700

    add some style

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ var async = require('async');
 var itemTpl = require('./templates/list-item.hbs');
 
 var myFirebaseRef = new Firebase("https://hacker-news.firebaseio.com/v0/");
-
+var itemsContainer = document.querySelector('.items');
 
 function display (snapshot) {
 	var top = snapshot.val().slice(0, 30);
 	async.map(top, getItem, function(err, items) {
 		items.forEach(function(item) {
-			document.write(itemTpl(item));
+			itemsContainer.innerHTML += itemTpl(item);
 		});
 	});
 }
