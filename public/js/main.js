5d6074e87ed8428c0840f1c40ab1f943d9b6ea5a
Bo Lopker <blopker@23andme.com>
2016-06-22 11:37:41 +0200

    no A

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 (function() {
     document.addEventListener('click', function(e) {
+        if (e.target.tagName === 'A') { return; }
         const comment = e.target.closest('.js-comment');
         if (comment) {
             comment.classList.toggle('collapse');
